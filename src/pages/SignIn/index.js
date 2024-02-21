ab13efd77daac0cca1ec78c77993a9143c2b4629
Andrea Ribeiro <102726192+Andrea-Ribeiro@users.noreply.github.com>
2024-02-21 15:21:31 +0200

    Initializing orders project

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,7 @@
+export default function  StaticRangeInit(){
+    return(
+        <div>
+            <h1>Pagina login</h1>
+        </div>
+    )
+}
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,9 @@
+export default function SignUp(){
+    return(
+        <div>
+            <h1>
+                Tela de cadastro
+            </h1>
+        </div>
+    )
+}
